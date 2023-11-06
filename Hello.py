883b12766408109b17b343d5ed666b050c96e545
DarioV4 <149717717+DarioV4@users.noreply.github.com>
2023-11-06 00:12:04 +0000

    Version 1.16

--- Hello.py
+++ Hello.py
@@ -25,7 +25,7 @@ class Model:
     def predict(self, instances):
         predictions = []
         for i in instances:
-            predictions.append(2)
+            predictions.append(0)
        
         return np.array(predictions)
 
